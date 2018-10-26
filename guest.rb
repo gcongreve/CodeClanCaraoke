166c83106a80474b516852287b1b520a5b907b88
Graeme Congreve <junkmailgmc@gmail.com>
2018-10-26 14:46:30 +0100

    Added money to and show_money to Guest Class

--- guest.rb
+++ guest.rb
@@ -1,9 +1,15 @@
 class Guest
 
-  attr_reader :name 
+  attr_reader :name
 
-  def initialize(name)
+  def initialize(name, cash_amount)
     @name = name
+    @cash_amount = cash_amount
+  end
+
+
+  def show_money
+    return @cash_amount
   end
 
 
--- spec/guest_spec.rb
+++ spec/guest_spec.rb
@@ -5,11 +5,15 @@ require_relative('../guest')
 class GuestTest < MiniTest::Test
 
   def setup
-    @guest1 = Guest.new("Adam")
+    @guest1 = Guest.new("Adam", 10)
   end
 
   def test_guest_has_name
     assert_equal("Adam", @guest1.name)
+  end
+
+  def test_guest_has_money
+    assert_equal(10, @guest1.show_money)
   end
 
 end
